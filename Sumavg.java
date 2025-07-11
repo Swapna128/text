1f04f79edfd16a37760fbabef9e279d54c1dc745
Swapna128 <talishettyuma@gmail.com>
2025-07-11 12:36:21 +0530

    Add files via upload

---
+++ Sumavg.java
@@ -0,0 +1,14 @@
+class Sumavg
+{
+public static void main(String args[])
+{
+int sum=0,avg;
+for(int i=0;i<11;i++)
+{
+sum=sum+i;
+}
+System.out.println("Total sum:"+sum);
+avg=sum/10;
+System.out.println("average:"+avg);
+}
+}
